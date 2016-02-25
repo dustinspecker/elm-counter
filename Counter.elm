7b952bbd01485f8f40b86f5ececc38ca2f66dbb9
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-02-24 18:34:09 -0600

    chore(all): add type info

--- Counter.elm
+++ Counter.elm
@@ -2,8 +2,10 @@ module Counter where
 
 import Html exposing (..)
 
+view : Signal.Address Int -> Int -> Html
 view address model =
   text (toString model)
 
+update : Int -> Int -> Int
 update action model =
   model
--- Main.elm
+++ Main.elm
@@ -1,8 +1,10 @@
 module Main where
 
 import Counter exposing (update, view)
+import Html
 import StartApp.Simple exposing (start)
 
+main : Signal Html.Html
 main =
   start
     { model = 0
